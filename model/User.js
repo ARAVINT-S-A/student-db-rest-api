4133ca356f08864d01f7caf40aa947ee0c3c77cc
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-31 15:19:59 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const bcrypt=require('bcryptjs')
 const validator=require('validator')
-const CoursesSchema=require('./courses').schema
+
 
 const UserSchema=new mongoose.Schema({
     name:{

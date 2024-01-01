43afcd828672f1bf9d274d8212e4403066c1151c
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2024-01-01 18:17:53 +0530

    Create courseController.js

---
+++ controllers/courseController.js
@@ -0,0 +1,19 @@
+const {StatusCodes}=require('http-status-codes')
+const CustomError=require('../errors/index')
+const Courses=require('../model/courses')
+
+const registerCourse=async(req,res)=>{
+    
+}
+
+const getAllCourses=async(req,res)=>{
+
+}
+
+const viewRegisteredCourses=async(req,res)=>{
+
+}
+
+module.exports={registerCourse,
+getAllCourses,
+viewRegisteredCourses}

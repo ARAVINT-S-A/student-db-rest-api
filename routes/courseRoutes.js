eaadc355d6dd96f8d7aa5d217aa20f4fbcc8d3cd
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2024-01-01 18:26:02 +0530

    Update courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const router=express.Router()
 
-const{registerCourse,getAllCourses,viewRegisteredCourses}=require('../controllers/courseController')
+const{registerCourse,getAllCourses,viewRegisteredCourses,editCourseDetails}=require('../controllers/courseController')
+const {authenticateUser,authorizePermissions}=require('../middleware/authentication')
 
-router.route('/registercourse').post(registerCourse)
+router.route('/registercourse').post(authenticateUser,registerCourse)
 router.route('/').get(getAllCourses)
-router.route('/viewregistedcourses').get(viewRegisteredCourses)
+router.route('/viewregistedcourses').get(authenticateUser,viewRegisteredCourses)
+router.route('/:id').patch(authenticateUser,authorizePermissions('admin'),editCourseDetails)
 
 module.exports=router
